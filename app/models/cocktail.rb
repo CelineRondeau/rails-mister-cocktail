753916a67519d98bfc26066a5c75e1a753b8cfa7
CelineRondeau <rondeau9314@gmail.com>
2020-05-14 15:56:34 -0400

    Fixing errors in validations

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,12 +1,10 @@
 class Cocktail < ApplicationRecord
-  :name = [ "Mint Julep", "Whiskey Sour", "Mojito" ]
-  
-  has_many :ingredient
+  has_many :ingredients
   # A cocktail has a name (e.g. "Mint Julep", "Whiskey Sour", "Mojito")
   # A cocktail must have a unique name.
-  validates :name, presence: true, uniqueness :true
+  validates :name, presence: true, uniqueness: true
   # A cocktail has many doses
   # A cocktail has many ingredients through doses
   # When you delete a cocktail, you should delete associated doses (but not the ingredients as they can be linked to other cocktails).
-  has_many :dose, dependent: :destroy
+  has_many :doses, dependent: :destroy
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,6 @@
 class Dose < ApplicationRecord
   # A dose must have a description, a cocktail and an ingredient, and [cocktail, ingredient] pairings should be unique
-  validates :name, presence: true, uniqueness :true
+  validates :name, presence: true, uniqueness: true
   belongs_to :cocktail
   belongs_to :ingredient
 end
