9991816062d437753be13ef086101b7a5874f9b5
CelineRondeau <rondeau9314@gmail.com>
2020-05-14 15:44:27 -0400

    Working on validation

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,6 @@
 class Dose < ApplicationRecord
+  # A dose must have a description, a cocktail and an ingredient, and [cocktail, ingredient] pairings should be unique
+  validates :name, presence: true, uniqueness :true
   belongs_to :cocktail
   belongs_to :ingredient
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,8 @@
 class Ingredient < ApplicationRecord
+  has_many :dose, dependent: :destroy
+  # An ingredient must have a unique name
+  # An ingredient has a name (e.g. "lemon", "ice", "mint leaves")
+  validates :name, presence: true, uniqueness :true
+  # An ingredient has many doses
+
 end
